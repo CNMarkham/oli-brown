0e91eb57f553b8d15e611c28fc9a37b7a6104201
Madelyn <markhamonca@codeninjas.com>
2024-01-28 15:59:08 -0500

    01/28/24

---
+++ oli-jungle-escape-py/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    bool hit;
+    void Update()
+    {
+        hit = Physics.Raycast(transform.position, transform.forward, 2f);
+        Debug.DrawRay(transform.position, transform.forward * 2f, Color.red);
+        if (hit == true)
+        {
+            Debug.LogWarning("Be careful!");
+        }
+        else
+        {
+            Debug.LogWarning("All clear!");
+        }
+    }
+}
